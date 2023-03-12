f66f19c517a783b71372fb5be9258a3367d4b569
Khánh Nguyễn Võ <khanhyou2018@gmail.com>
2023-03-12 15:42:59 +0700

    add void Compare()

--- src/pInToPos/InfixToPosfix.java
+++ src/pInToPos/InfixToPosfix.java
@@ -1,7 +1,12 @@
 package pInToPos;
 
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
+
+
 
 public class InfixToPosfix {
 
@@ -11,33 +16,97 @@ public class InfixToPosfix {
 		//"$" < "(" < "+" = "-" < "*" = "/" < "^"
 		
 		Scanner scn = new Scanner(System.in);
-		
+		Stack<String> stack = new Stack<String>();
+		Hashtable<String,Integer> htbSignMath = new Hashtable<String,Integer>();
+
 		String Input = scn.nextLine();
+		
+		String Result="";
 		
 		String Character = "a, b, c, d, e, f, g, h, i, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
 		String signMath ="$,(,+,-,*,/,^";
 		String numFlowMath="0,1,2,3,4,5,6";
 		
 		String[] partCharacter =Character.split(", ");
-		String[] partsignMath=signMath.split(",");
-		String[] partnumFlowMath=numFlowMath.split(",");
+		String[] partSignMath=signMath.split(",");
+		String[] partNumFlowMath=numFlowMath.split(",");
 		String[] partInput =Input.split("");
-
 		
-		for(int i = partInput.length;i>=0;i--) {
-			System.out.println(i);
+		//Send item in hashtable 
+		for(int i=0;i<partSignMath.length;i++) {
+			htbSignMath.put(partSignMath[i],i);
 		}
 		
 		
-		Stack<String> stack = new Stack<String>();
+
+
+		
+		for(int i = partInput.length-1 ; i>=0 ; i--) {
+			
+			for(int j=partSignMath.length-1 ; j>=0 ; j-- ) {
+				
+				if(partInput[i]==partSignMath[j])
+				{
+					
+				}
+				
+				if(partInput[i]==partSignMath[j]) {
+					//stack.push(partInput[i]);
+				}
+					
+			}
+			
+			
+		}
 		
 		
 		
+		
+		
+		CompareSignMath("3",htbSignMath);
 		
 		
 		System.out.println("Input_infix:");
 		String input = scn.nextLine();
 		
 	}
+	
+	
+	public static void CompareSignMath(String selectSignMath,Hashtable<String,Integer> htb) {
+		
+		try {
 
+			int valueInput = htb.get(selectSignMath);
+			int check=0;
+			
+			Map.Entry<String,Integer>[] array = htb.entrySet().toArray(new Map.Entry[0]);
+			
+			//"$" < "(" < "+" = "-" < "*" = "/" < "^"
+			//0,1,2,3,4,5,6
+			// Duyệt qua các phần tử trong Hashtable bằng Iterator
+					for(int i=0;i<array.length;i++) {
+						if(htb.get(selectSignMath)>array[i].getValue() || htb.get(selectSignMath)==array[i].getValue() ) {
+							check=0;
+						}
+						else {
+							System.out.println(""+htb.get(selectSignMath)+"<"+array[i].getValue());
+							check=1;
+			
+								}
+					}
+						
+					System.out.println("Check="+check);
+					
+					for(int i =0;i<7;i++) {
+						System.out.println(array[i].getValue()+""+array[i].getKey());
+					}
+		
+		 
+		
+					
+			}
+			finally {System.out.println("Lỗi không có trong SignMath");}
+						
+		
+	}
 }
